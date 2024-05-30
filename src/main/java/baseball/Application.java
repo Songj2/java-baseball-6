34668fbed37d821a68987d0a627662f20887cb64
SongJIH <wlgus7592@gmail.com>
2024-05-30 10:14:09 +0900

    중복없는 임의의 숫자 3개 뽑기()

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,45 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String userNumber;
+        boolean numberMatched = false;
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        List<Integer> answer= randomNumber();
+
     }
+
+    //    숫자 3개 뽑기
+    public static List<Integer> randomNumber() {
+        List<Integer> list = new ArrayList<>();
+        while (list.size() < 3) {
+            int randomNum = Randoms.pickNumberInRange(1, 9);
+            if (!list.contains(randomNum)) {
+                list.add(randomNum);
+            }
+        }
+        return list;
+    }
+//    숫자 3개 입력 받기
+
+    //    유효성 검사
+//    1. 숫자인가
+//    2. 중복인가
+
+//    숫자 비교하기
+
+//    자리 비교하기
+
+//    힌트/결과 출력하기
+
+//    재시작 혹은 종료
 }
